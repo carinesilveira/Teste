6c45a868733b02bf1e4900591a52aae52fc9151b
unknown <ANGianini>
2024-10-17 16:26:41 -0300

    adicionando Andressa

--- ConhecendoGiteGirhub/src/CloneePull.java
+++ ConhecendoGiteGirhub/src/CloneePull.java
@@ -3,7 +3,7 @@ public class CloneePull {
 
 	public static void main(String[] args) {
 		System.out.println("Conhecendo os comandos git glone e git pull");
-		System.out.println("1 - ");
+		System.out.println("1 - Andressa S2");
 		System.out.println("2 - ");
 		System.out.println("3 - ");
 		System.out.println("4 - ");
